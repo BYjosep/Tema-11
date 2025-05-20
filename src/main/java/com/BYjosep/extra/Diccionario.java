970dbd364e93c99a9c75ba3dccf7e7b9d807fdc7
Josep Vicent Mestre Llobell <59211313+BYjosep@users.noreply.github.com>
2025-05-20 10:48:22 +0200

    feat: Implementa traducción de texto a Morse
    Implementa la traducción de texto a código Morse
    utilizando un diccionario.

--- src/main/java/com/BYjosep/extra/Diccionario.java
+++ src/main/java/com/BYjosep/extra/Diccionario.java
@@ -1,19 +1,63 @@
 package com.BYjosep.extra;
 
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
 
 public class Diccionario {
     public final Map<Character, String> diccionario = new HashMap<Character, String>();
 
-    private void ponerValoresEnDiccionario() {
-        final char[] caracteres = {'a','b','c','d','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y'}
-         final String[] morse = {"dfsa","sd"};
+        public static final Map<Character, String> MORSE_MAP;
 
-        for (int i = 0; i < caracteres.length; i++) {
-            diccionario.put(caracteres[i], morse[i]);
+        static {
+            Map<Character, String> morseTemp = new HashMap<>();
+
+            // Letras del abecedario español en minúsculas
+            morseTemp.put('a', ".- ");
+            morseTemp.put('b', "-... ");
+            morseTemp.put('c', "-.-. ");
+            morseTemp.put('d', "-.. ");
+            morseTemp.put('e', ". ");
+            morseTemp.put('f', "..-. ");
+            morseTemp.put('g', "--. ");
+            morseTemp.put('h', ".... ");
+            morseTemp.put('i', ".. ");
+            morseTemp.put('j', ".--- ");
+            morseTemp.put('k', "-.- ");
+            morseTemp.put('l', ".-.. ");
+            morseTemp.put('m', "-- ");
+            morseTemp.put('n', "-. ");
+            morseTemp.put('ñ', "--.-- ");
+            morseTemp.put('o', "--- ");
+            morseTemp.put('p', ".--. ");
+            morseTemp.put('q', "--.- ");
+            morseTemp.put('r', ".-. ");
+            morseTemp.put('s', "... ");
+            morseTemp.put('t', "- ");
+            morseTemp.put('u', "..- ");
+            morseTemp.put('v', "...- ");
+            morseTemp.put('w', ".-- ");
+            morseTemp.put('x', "-..- ");
+            morseTemp.put('y', "-.-- ");
+            morseTemp.put('z', "--.. ");
+
+            // Números del 0 al 9
+            morseTemp.put('0', "----- ");
+            morseTemp.put('1', ".---- ");
+            morseTemp.put('2', "..--- ");
+            morseTemp.put('3', "...-- ");
+            morseTemp.put('4', "....- ");
+            morseTemp.put('5', "..... ");
+            morseTemp.put('6', "-.... ");
+            morseTemp.put('7', "--... ");
+            morseTemp.put('8', "---.. ");
+            morseTemp.put('9', "----. ");
+
+            // Caracteres especiales
+            morseTemp.put(' ', "/ ");
+            morseTemp.put(',', "--..-- ");
+            morseTemp.put('.', ".-.-.- ");
+
+            MORSE_MAP = Collections.unmodifiableMap(morseTemp);
         }
     }
-
-
-}
--- src/main/java/com/BYjosep/extra/TextoAMorse.java
+++ src/main/java/com/BYjosep/extra/TextoAMorse.java
@@ -1,5 +1,48 @@
 package com.BYjosep.extra;
 
+import com.BYjosep.Config;
+import com.BYjosep.Exeptions.FileFoundExeption;
+
+import java.io.*;
+
 public class TextoAMorse {
+
+    private static final String SALIDA = Config.FILE_PATH+"Traduccion_a_morse.txt";
+    public static void main(String[] args) {
+        try {
+            Config.comprobarArg(args,1,true);
+        } catch (FileNotFoundException e) {
+            System.out.println(e.getMessage());
+            System.exit(-1);
+        } catch (FileFoundExeption e) {
+            System.out.println(e.getMessage());
+            System.exit(1);
+        }
+        File original = new File(args[0]);
+
+        traduccionAMorse(original);
+
+        System.out.println("Se ha terminado co en proceso");
+    }
+
+    private static void traduccionAMorse(File original) {
+        File traduccion = new File(SALIDA);
+
+        try(BufferedReader br = new BufferedReader(new FileReader(original));
+        BufferedWriter bw = new BufferedWriter(new FileWriter(traduccion))) {
+
+            String line;
+            while ((line = br.readLine()) != null) {
+                for ( char caracter: line.toLowerCase().toCharArray()){
+                    bw.write(Diccionario.MORSE_MAP.getOrDefault(caracter,caracter+" "));
+                }
+                bw.newLine();
+            }
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
 
 }
